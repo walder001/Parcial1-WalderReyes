ca489ac463166137d477e69f082d39c902c96325
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-05-30 18:55:42 -0400

    Programacion del metodo Buscar de la clase ProductoBLL.

--- BLL/ProductoBLL.cs
+++ BLL/ProductoBLL.cs
@@ -10,6 +10,47 @@ namespace Parcial1_WalderReyes.BLL
 {
     public class ProductoBLL
     {
+       
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos productos = new Productos();
+            try
+            {
+                productos = contexto.Productos.Find(id);
+            }
+            catch
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return productos;
+        }
+
+        public static bool buscar(Productos productos)
+        {
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var buscar = contexto.Productos.Find(productos.ProductoId);
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return buscar;
+        }
         public static bool Guardar(Productos productos)
         {
             bool paso = false;
